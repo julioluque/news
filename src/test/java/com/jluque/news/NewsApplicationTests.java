d7d877335ad5930d038701fdd77fdaca356f6dcc
Julio Luque <julio.a.luque@gmail.com>
2020-07-15 03:57:55 -0300

    Commit inicial, Creacion de proyecto Spring Boot

---
+++ src/test/java/com/jluque/news/NewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jluque.news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
